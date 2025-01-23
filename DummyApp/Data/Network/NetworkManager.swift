58c98003c4c5921c83817aa56b1bef99b16336b3
Shashi Kumar <shakumar69@WKMZT2CDBF56.global.publicisgroupe.net>
2025-01-23 23:59:00 +0530

    remove commented code

--- DummyApp/Data/Network/NetworkManager.swift
+++ DummyApp/Data/Network/NetworkManager.swift
@@ -6,62 +6,6 @@
 import Foundation
 import Combine
 
-
-// Concrete implementation of NetworkService using Combine
-//class NetworkManager: NetworkService {
-//    private let session: URLSession
-//    
-//    init(session: URLSession = .shared) {
-//        self.session = session
-//    }
-//    
-//    func request<T: Decodable>(endpoint: APIEndpoint) -> AnyPublisher<T, NetworkError> {
-//        guard let request = endpoint.urlRequest() else {
-//            return Fail(error: NetworkError.badURL).eraseToAnyPublisher()
-//        }
-//        
-//        return session.dataTaskPublisher(for: request)
-//            .map { $0.data }
-//            .decode(type: T.self, decoder: JSONDecoder())
-//            .mapError { error in
-//                (error as? URLError) != nil ? NetworkError.requestFailed(error) : NetworkError.decodingError
-//            }
-//            .eraseToAnyPublisher()
-//    }
-//}
-
-//class NetworkManager: NetworkService {
-//    private let session: URLSession
-//    
-//    init(session: URLSession = .shared) {
-//        self.session = session
-//    }
-//    
-//    func request<T: Decodable>(endpoint: APIEndpoint) -> AnyPublisher<T, NetworkError> {
-//        guard let request = endpoint.urlRequest() else {
-//            print("Invalid URL request")
-//            return Fail(error: NetworkError.badURL).eraseToAnyPublisher()
-//        }
-//        
-//        print("Making request to URL: \(request.url?.absoluteString ?? "Unknown URL")")
-//        
-//        return session.dataTaskPublisher(for: request)
-//            .tryMap { output in
-//                guard let response = output.response as? HTTPURLResponse,
-//                      (200...299).contains(response.statusCode) else {
-//                    throw NetworkError.invalidResponse
-//                }
-//                return output.data
-//            }
-//            .decode(type: T.self, decoder: JSONDecoder())
-//            .mapError { error in
-//                print("Network error: \(error)")
-//                return (error as? URLError) != nil ? NetworkError.requestFailed(error) : NetworkError.decodingError
-//            }
-//            .eraseToAnyPublisher()
-//    }
-//}
-
 class NetworkManager: NetworkService {
     private let session: URLSession
     
